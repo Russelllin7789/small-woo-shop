acb5a38d13cfce8126598dbd80cabe0e3cc97425
RussellLin <superbr7789@gmail.com>
2021-11-28 22:00:37 +0800

    add WooCommerceRestApi data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React, { useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
+
+const WooCommerce = new WooCommerceRestApi({
+  url: 'http://localhost:8888/',
+  consumerKey: 'ck_98e50c0bbe1677f6b81ded7b5e00e8dfe09fdb60',
+  consumerSecret: 'cs_d4ea54a6da5ab2f5fae1e204bf3a95b00aa23497',
+  version: 'wc/v3'
+})
 
 function App() {
+  useEffect(() => {
+    WooCommerce.get("products")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Learn React</p>
     </div>
   );
 }
