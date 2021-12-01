99df92a319c39a9fdf15548442ca254307073cc6
RussellLin <superbr7789@gmail.com>
2021-12-01 08:36:51 +0800

    Add Google materials for homePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import './App.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+
+import TopAppBar, {
+  TopAppBarIcon,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+} from '@material/react-top-app-bar';
+
+import MaterialIcon from '@material/react-material-icon';
+import Drawer from '@material/react-drawer';
+import List, { ListItem } from '@material/react-list';
+
 import HomePage from './view/home/homePage'
 import Nav from './view/layout/nav'
 
 const App = () => {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Drawer
+        modal
+      >
+        <Nav />
+      </Drawer>
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection align='start'>
+            <TopAppBarIcon navIcon tabIndex={0}>
+              <MaterialIcon hasRipple icon='menu' />
+            </TopAppBarIcon>
+            <TopAppBarTitle>
+              線上商城
+            </TopAppBarTitle>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <div className="App">
+        <HomePage />
+      </div>
+    </Router>
   )
 }
 
