c2c068d436a175e9570a1069f41d7b73adb8b493
RussellLin <superbr7789@gmail.com>
2021-12-03 08:40:28 +0800

    Add service for products

---
+++ src/services/productService.js
@@ -0,0 +1,25 @@
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
+
+const WooCommerce = new WooCommerceRestApi({
+  url: 'http://localhost:8888/',
+  consumerKey: 'ck_98e50c0bbe1677f6b81ded7b5e00e8dfe09fdb60',
+  consumerSecret: 'cs_d4ea54a6da5ab2f5fae1e204bf3a95b00aa23497',
+  version: 'wc/v3'
+})
+
+class ProductService {
+  getProducts = (page) => {
+    return WooCommerce.get('products', {
+      page: page,
+      per_page: 3
+    })
+      .then((response) => {
+        setProducts(response.data)
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+      })
+  }
+}
+
+export default ProductService
--- src/view/products/productsIndexPage.jsx
+++ src/view/products/productsIndexPage.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ProductService from "../../services/productService";
+
+const productService = new ProductService()
 
 const ProductsIndexPage = () => {
+
+  const [products, serProducts] = useState([])
+
+  productService.getProducts(1)
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <h1>ProductsIndexPage</h1>
   )
