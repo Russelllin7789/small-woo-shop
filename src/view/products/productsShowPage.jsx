721e822b06cbe44d68d00679734c7c8b967b7907
RussellLin <superbr7789@gmail.com>
2021-12-13 21:48:59 +0800

    Add useParams into productsShowPage

--- src/view/products/productsShowPage.jsx
+++ src/view/products/productsShowPage.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  useParams,
+} from 'react-router-dom'
+import LoadingView from '../layout/loadingView'
+import ProductService from '../../services/productService'
+
+const productService = new ProductService()
 
 const ProductsShowPage = () => {
+  let { id } = useParams()
   return (
     <h1>ProductsShowPage</h1>
   )
