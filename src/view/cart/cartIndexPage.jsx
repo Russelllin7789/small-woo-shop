f0a5fd1c2dccf467fa1f0d30caf86389dac0f0ed
RussellLin <superbr7789@gmail.com>
2021-12-21 08:15:21 +0800

    Fulfill cartIndexPage

--- src/view/cart/cartIndexPage.jsx
+++ src/view/cart/cartIndexPage.jsx
@@ -1,8 +1,47 @@
 import React from "react";
+import {
+  Link
+} from 'react-router-dom';
+import {
+  Cell,
+  Grid,
+  Row
+} from '@material/react-layout-grid';
+import Button from "@material/react-button";
+import CartItemsList from "./component/cartItemsList";
 
 const CartIndexPage = () => {
   return (
-    <h1>cartIndexPage</h1>
+    <div>
+      <h1>cartIndexPage</h1>
+      <Grid>
+        <Row>
+          <Cell
+            desktopColumns={8}
+            phoneColumns={4}
+            tabletColumns={8}
+          >
+            <div style={{ padding: '16px 32px' }}>
+              <CartItemsList />
+            </div>
+          </Cell>
+          <Cell
+            desktopColumns={4}
+            phoneColumns={4}
+            tabletColumns={8}
+          >
+            <div style={{ paddingTop: '32px' }}>
+              <p>總價：{0}</p>
+              <div>
+                <Link to='/checkout'>
+                  <Button outlined>結帳</Button>
+                </Link>
+              </div>
+            </div>
+          </Cell>
+        </Row>
+      </Grid>
+    </div>
   )
 }
 
