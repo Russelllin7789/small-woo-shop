024fd493385d48186435d55d98eca700ee97d092
RussellLin <superbr7789@gmail.com>
2021-12-25 15:16:26 +0800

    Fulfill orderFailed page

--- src/view/orders/orderFailedPage.jsx
+++ src/view/orders/orderFailedPage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import {
+  Link
+} from 'react-router-dom'
 
 const OrdersFailedPage = () => {
   return (
-    <h1>orderFailedPage</h1>
+    <div style={{ padding: '88px 0', textAlign: 'center' }}>
+      <h1>This order was failed! T______T</h1>
+      <div style={{ paddingTop: '128px' }}>
+        <Link to='/'>
+          Back to HomePage
+        </Link>
+      </div>
+    </div>
   )
 }
 
