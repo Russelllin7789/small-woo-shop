22400cbf2d5ed497a54e70c2b85db5a2a7f2136c
RussellLin <superbr7789@gmail.com>
2021-12-28 07:09:40 +0800

    Add customerService.js

---
+++ src/services/customerService.js
@@ -0,0 +1,86 @@
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import Customer from '../models/customer'
+import Cookies from "js-cookie";
+
+const CUSTOMER_KEY = 'customer'
+
+const WooCommerce = new WooCommerceRestApi({
+  url: 'http://localhost:8888/',
+  consumerKey: 'ck_6fac49b6cc20e4df9df284b32e281dad767c3cd9',
+  consumerSecret: 'cs_74afc150c7ceaa7ae5a24cbdc48fcb7cbe07b0c4',
+  version: 'wc/v3'
+})
+
+class CustomerService {
+  constructor() {
+    this.customerStorage = Cookies.get(CUSTOMER_KEY)
+    if (this.customerStorage === null) {
+      this.clearCustomerStorage()
+    } else {
+      this.customerStorage = JSON.parse(this.customerStorage)
+    }
+  }
+
+  clearCustomerStorage = () => {
+    this.customerStorage = {}
+    this.saveToCustomerStorage()
+  }
+
+  saveToCustomerStorage = () => {
+    Cookies.set(CUSTOMER_KEY, this.customerStorage, { expires: 7 })
+  }
+
+  setCustomerIdToCookie = (customerId) => {
+    this.customerStorage['customerId'] = customerId
+    this.saveToCustomerStorage()
+  }
+
+  isLoggedIn = () => {
+    return this.getCustomerIdFromCookie() !== null
+  }
+
+  getCustomerIdFromCookie = () => {
+    return this.customerStorage['customerId']
+  }
+
+  // verify if the 'customer' we got is the right data format
+  getCustomerById = (id) => {
+    return WooCommerce.get(`customers/${id}`)
+      .then((response) => {
+        const customer = new Customer(response.data)
+        this.setCustomerIdToCookie(customer.id)
+        return customer
+      })
+      .catch((error) => {
+        console.log(error)
+        return null
+      })
+  }
+
+  logIn = (email) => {
+    return WooCommerce.get('customers', {
+      email: email,
+      role: 'all'
+    })
+      .then((response) => {
+        if (response.data.length > 0) {
+          const customer = new Customer(response.data[0])
+          this.setCustomerIdToCookie(customer.id)
+          return customer
+        } else {
+          return null
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        return null
+      })
+  }
+
+  logOut = () => {
+    this.customerStorage['customerId'] = null
+    this.saveToCustomerStorage()
+  }
+}
+
+export default CustomerService
--- src/view/customer/logInPage.jsx
+++ src/view/customer/logInPage.jsx
@@ -23,6 +23,54 @@ function LogInPage() {
     )
   }
 
+  return (
+    <Grid>
+      <Row>
+        <Cell></Cell>
+        <Cell>
+          <div style={{ padding: '80px 0px' }}>
+            <h1 style={{ textAlign: 'center', paddingBottom: '24pxpx' }}></h1>
+            <div>
+              <TextField
+                outlined
+                label='Login Email'
+                style={{ width: '100%' }}
+              >
+                <Input
+                  name='email'
+                  type='email'
+                  value={uiStatus.email}
+                  onChange={typeInInput}
+                />
+              </TextField>
+            </div>
+            <br />
+            <div>
+              <TextField
+                outlined
+                label='Login Password'
+                style={{ width: '100%' }}
+              >
+                <Input
+                  name='password'
+                  type='password'
+                  value={uiStatus.password}
+                  onChange={typeInInput}
+                />
+              </TextField>
+            </div>
+            <br />
+            <br />
+            {
+              uiStatus.isLoading ? (<Button disabled={true}>Log In</Button>) : (<Button onClick={tryToLogin}>Log In</Button>)
+            }
+          </div>
+        </Cell>
+        <Cell></Cell>
+      </Row>
+    </Grid>
+  )
+
 }
 
 export default LogInPage
